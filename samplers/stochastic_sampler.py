9c7fda4105716b8ac09413a9a4fab7cc75200106
Amal Dev Haridevan <haridevanamaldev@gmail.com>
2025-03-31 18:09:23 -0400

    added doc

--- samplers/stochastic_sampler.py
+++ samplers/stochastic_sampler.py
@@ -6,6 +6,18 @@ import torch
 import numpy as np
 
 class StochasticSampler:
+    """In their paper, they used a scheduling function sigma(t) = t,
+    and scaling function s(t) = 1.0
+    For other configurations:
+        VP: sigma(t) = sqrt(exp(0.5*beta_d*t^2 + beta_min*t) -1), where beta_d = 19.9, and beta_min = 0.1
+            s(t) = 1/sqrt(exp(0.5*beta_d*t^2 + beta_min*t))
+        VE: sigma(t) = sqrt(t)
+            s(t) = 1
+        iDDPM + DDIM:
+            sigma(t) = t
+            s(t) = 1
+        edm (this):
+            same as iDDPM + DDIM"""
     def __init__(self, dims, max_N):
         self.dims = dims
         self.max_N = max_N
