dc11374cd9729c2d0a61f5f40d5c8b29beb18d79
Amal Dev Haridevan <haridevanamaldev@gmail.com>
2025-04-11 16:54:08 -0400

    added table

--- samplers/stochastic_sampler.py
+++ samplers/stochastic_sampler.py
@@ -17,7 +17,30 @@ class StochasticSampler:
             sigma(t) = t
             s(t) = 1
         edm (this):
-            same as iDDPM + DDIM"""
+            same as iDDPM + DDIM
+    NOTE: The SDE of Song et al can be generalized as a sum of probability flow of ODE in eq.1 of the paper
+     and a time varying Langevin diffusion SDE
+    The algorithm for sampling is as follows:
+        Given sample x_i at noise level sigma(t_i), there are two substeps
+        1, Add noise to x_i based on a factor gamma_i >= 0 to increase the noise level
+        where t_i^ = t_i + gamma_i*t_i . From this higher noise level, sample x^_i.
+        2. Solve the ODE backwards from t^i to t_i+1 in a single step
+        The output is a sample x_i+1 at noise level t_i+1
+    
+    In table 5.0, the authors list the sampling parameters for the stochastic sampler
+    Which are listed below:
+    =====================================================
+    Param           CIFAR-10        ImageNet
+    =====================================================
+    |               VP  |  VE      Pre-trained | This
+    |====================================================
+    | S_churn       30     80       80            40    |
+    | S_tmin        0.01    0.05    0.05          0.05  |
+    | S_tmax        1       1       50            50    |
+    | S_nosie       1.007   1.007   1.003         1.003 |
+    =====================================================
+    
+        """
     def __init__(self, dims, max_N):
         self.dims = dims
         self.max_N = max_N
